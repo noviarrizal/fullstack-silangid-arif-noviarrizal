05a4acaf96ed75c668fdf482ee8407b8ea7c8425
arif noviarrizal <muhammad.noviarrizal@ist.id>
2025-05-03 01:53:41 +0700

    initialise react frontend

---
+++ server/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminae\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('users', UserController::class);
+});
